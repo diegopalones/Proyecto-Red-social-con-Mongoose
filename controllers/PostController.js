5b5016782ba23140e40c5207ce51379444148f9b
diegopalones <diego.palones@gmail.com>
2022-11-10 16:17:33 +0100

    creating posts working

---
+++ controllers/PostController.js
@@ -0,0 +1,82 @@
+const Post = require("../models/Post");
+
+const PostController = {
+  async create(req, res) {
+    try {
+      const post = await Post.create(req.body);
+      res.status(201).send(post);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send({ message: "Ha habido un problema al crear la publicación" });
+    }
+  },
+
+  async getAll(req, res) {
+    try {
+      const posts = await Post.find();
+      res.send(posts);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async getById(req, res) {
+    try {
+      const post = await Post.findById(req.params._id);
+      res.send(post);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async getPostsByUserName(req, res) {
+    try {
+      if (req.params.username.length > 20) {
+        return res.status(400).send("Busqueda demasiado larga");
+      }
+      const username = new RegExp(req.params.username, "i");
+      const post = await Post.find({ username });
+      res.send(post);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      const post = await Post.findByIdAndDelete(req.params._id);
+      res.send({ post, message: "Publicación borrada" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Hubo un problema al eliminar la publicación",
+      });
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      const post = await Post.deleteMany(req.params.username);
+      res.send({ post, message: "Publicaciones eliminadas" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Hubo un problema al eliminar las publicaciones",
+      });
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const post = await Post.findByIdAndUpdate(req.params._id, req.body, {
+        new: true,
+      });
+      res.send({ message: "Publicación actualizada correctamente", post });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+module.exports = PostController;
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = 8080;
 
 const { dbConnection } = require("./config/config")
 
 app.use(express.json())
 
-app.use("/users", require("./routes/users"));
-
 dbConnection()
+
+
+app.use("/users", require("./routes/users"));
+app.use("/posts", require("./routes/posts"));
+
+
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
